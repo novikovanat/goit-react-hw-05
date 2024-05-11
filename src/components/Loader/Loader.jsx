48ccd320d2e277151e32d6038ba6bdf52fa59f70
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-05-11 12:50:01 +0200

    loader added

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,19 @@
+import { Blocks } from "react-loader-spinner";
+
+export default function Loader() {
+  return (
+    <div>
+      (
+      <Blocks
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        visible={true}
+      />
+      )
+    </div>
+  );
+}
