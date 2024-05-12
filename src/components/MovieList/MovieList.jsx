018da071b8c739cf8130ad91c5e09a422fee8c9c
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-05-12 21:17:30 +0200

    error message addded

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function MovieList({ moviesArray }) {
-  const movieList = moviesArray.map(({ original_title, id }, index) => (
-    <li key={id}>
-      <Link to={`/movies/${id}`}>
-        <div>{original_title}</div>
-      </Link>
-    </li>
-  ));
+  console.log(location);
+  const movieList = moviesArray.map(({ original_title, id }, index) => {
+    const location = useLocation();
+    return (
+      <li key={id}>
+        <Link to={`/movies/${id}`} state={location}>
+          <div>{original_title}</div>
+        </Link>
+      </li>
+    );
+  });
   return <ul>{movieList}</ul>;
 }
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,7 +1,14 @@
+import { Link , useLocation } from "react-router-dom";
+
 export default function MovieDetailsPage() {
+  const location = useLocation();
+  
   return (
-    <h2>
-      MovieDetailsPage
-    </h2>
+    <div>
+      <Link to={location.state}>Go Back</Link>
+      <h2>
+        MovieDetailsPage
+      </h2>
+    </div>
   );
 }
